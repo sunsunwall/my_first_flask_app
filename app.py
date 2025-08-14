2617ce87330889330e99445406c34e5f40d3e018
Sunny Vanderwall <sunsunwall@gmail.com>
2025-08-14 09:43:29 +0200

    Added new route to app and added requirements file

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #the route component, in this case "/", 
+                # is the part of the URL that will run the code below (IE the function hello in this case)
 
 def hello():
     return "Hello, Welcome my flask_app!"
@@ -11,6 +12,11 @@ def hello():
 
 def goodbye():
     return "Goodbye from my flask_app!"
+
+@app.route('/aioverlord')
+
+def aioverlord():
+    return "Dont rely on LLMs for everything!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
